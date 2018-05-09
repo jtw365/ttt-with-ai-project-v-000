e5e0050e84bc2bace3be775e03e160bad2877d6d
Jonathan Williams <jtw365@live.missouristate.edu>
2018-05-09 02:01:48 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -36,7 +36,7 @@ module Players
         input = corner + 1
       else
         until !board.taken?(input)
-          input = (1..9).to_a.sample
+          input = (1..9).to_a.shuffle!
         end
       end
     end
